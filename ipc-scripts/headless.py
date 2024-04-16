b83e60db71705bf2951e101cbf1ceb414beecf89
Ilia Bozhinov <ammen99@gmail.com>
2024-04-16 11:37:15 +0200

    ipc-scripts/headless: rewrite with proper python argparse

--- ipc-scripts/headless.py
+++ ipc-scripts/headless.py
@@ -1,23 +1,34 @@
 import wayfire_socket as ws
 import os
-import sys
 import json
+import argparse
 
 addr = os.getenv('WAYFIRE_SOCKET')
 sock = ws.WayfireSocket(addr)
 
-if sys.argv[1] == "add":
-    output = sock.create_headless_output(int(sys.argv[2]), int(sys.argv[3]))
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='action')
+
+add = subparsers.add_parser('add')
+add.add_argument('width', nargs='?', type=int, default=1920)
+add.add_argument('height', nargs='?', type=int, default=1080)
+
+remove = subparsers.add_parser('remove')
+remove.add_argument('output')
+
+args = parser.parse_args()
+if args.action == "add":
+    output = sock.create_headless_output(int(args.width), int(args.height))
     if output and 'output' in output:
         print("Created headless output:\n" + json.dumps(output['output'], indent=4))
     else:
         print("Failed to create headless output: " + json.dumps(output, indent=4))
 
-elif sys.argv[1] == "remove":
-    if sys.argv[2].isdigit():
-        print(sock.destroy_headless_output(output_id=int(sys.argv[2])))
+elif args.action == "remove":
+    if args.output.isdigit():
+        print(sock.destroy_headless_output(output_id=int(args.output)))
     else:
-        print(sock.destroy_headless_output(output_name=sys.argv[2]))
+        print(sock.destroy_headless_output(output_name=args.output))
 
 else:
     print("Invalid usage, either add <width> <height> or remove <output-id|output-name>")
