1413b96993d09512145d1d428a23a08d00e5390e
tea <tea@teatwig.net>
2025-03-30 18:55:14 +0200

    basic overview positioning in a single row

--- plugins/single_plugins/qwf-overview.cpp
+++ plugins/single_plugins/qwf-overview.cpp
@@ -1,5 +1,7 @@
 #include "plugins/ipc/ipc-activator.hpp"
+#include "wayfire/geometry.hpp"
 
+#include <tuple>
 #include <wayfire/plugins/common/input-grab.hpp>
 #include <wayfire/plugins/common/util.hpp>
 
@@ -23,14 +25,20 @@
 #include <wayfire/window-manager.hpp>
 #include <wayfire/workspace-set.hpp>
 
-#include <algorithm>
-#include <glm/gtc/matrix_transform.hpp>
 #include <linux/input-event-codes.h>
+
+#include <algorithm>
+#include <functional>
 #include <memory>
+#include <numeric>
+#include <vector>
 
 constexpr const char *window_transformer_name = "qwf-overview";
 constexpr const char *background_transformer_name = "qwf-overview";
 constexpr float background_dim_factor = 0.6;
+constexpr float col_spacing = 0.0;
+constexpr float row_spacing = 0.0;
+constexpr float window_preview_max_scale = 0.95;
 
 using namespace wf::animation;
 class QWFOverviewPaintAttribs {
@@ -65,6 +73,13 @@ private:
     call(attribs.scale_x);
     call(attribs.scale_y);
   }
+};
+
+struct QWFOverviewRow {
+  float full_height, full_width;
+  float x, y, width, height;
+  float extra_scale;
+  std::vector<int> view_idxs;
 };
 
 class QWFOverview : public wf::per_output_plugin_instance_t, public wf::pointer_interaction_t {
@@ -222,7 +237,7 @@ public:
     }
 
     bool need_action = false;
-    for (auto &sv : views) {
+    for (const auto &sv : views) {
       need_action |= (sv.view == view);
     }
 
@@ -309,22 +324,6 @@ public:
     wf::scene::update(wf::get_core().scene(), wf::scene::update_flag::INPUT_STATE);
   }
 
-  // TODO this needs some algorithm for placement
-  /* Move view animation target */
-  void move(QWFOverviewView &sv) {
-    // interesting for positioning logic later
-    // auto og   = output->get_relative_geometry();
-    // auto bbox = wf::view_bounding_box_up_to(sv.view, window_transformer_name);
-
-    // these are timed_transition_t
-    // restart_with_end leaves the current state and only sets the end
-
-    constexpr float back_scale = 0.66;
-
-    sv.attribs.scale_x.restart_with_end(sv.attribs.scale_x.end * back_scale);
-    sv.attribs.scale_y.restart_with_end(sv.attribs.scale_y.end * back_scale);
-  }
-
   // returns a list of mapped views
   std::vector<wayfire_toplevel_view> get_workspace_views() const {
     return output->wset()->get_views(wf::WSET_MAPPED_ONLY | wf::WSET_CURRENT_WORKSPACE);
@@ -350,13 +349,249 @@ public:
       views.push_back(create_overview_view(v));
     }
 
+    // keep in case windows overlap for some reason and we need to focus the top one
+    // the computed layout does not modify the order
     std::sort(views.begin(), views.end(), [](QWFOverviewView &a, QWFOverviewView &b) {
       return wf::get_focus_timestamp(a.view) > wf::get_focus_timestamp(b.view);
     });
 
-    for (int i = 0; i < (int)views.size(); i++) {
-      move(views[i]);
+    // TODO _adjustSpacingAndPadding to set row_spacing and col_spacing
+
+    compute_layout(1);
+  }
+
+  /* This adapts the code from the Gnome "Activity Overview" to position the windows into rows and columns.
+   * https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/77d3a582abb336930c6c51725be2ad62794fe1e2/js/ui/workspace.js */
+  void compute_layout(int num_rows) {
+    assert(num_rows > 0);
+
+    float total_width = 0.0;
+    for (const auto &sv : views) {
+      const auto g = sv.view->get_geometry();
+      total_width += g.width * compute_window_scale(g);
     }
+
+    float ideal_row_width = total_width / (float)num_rows;
+
+    // TODO instead sort in the opposite direction so we can do pop_back to better iterate over them?
+
+    // generate indices for each window and sort them by the vertical window position
+    std::vector<int> view_idxs(views.size());
+    std::iota(view_idxs.begin(), view_idxs.end(), 0);
+    sort_vertical(view_idxs);
+
+    std::vector<QWFOverviewRow> rows;
+    int view_idx = 0;
+    for (int i = 0; i < num_rows; i++) {
+      QWFOverviewRow row{};
+
+      for (; view_idx < (int)view_idxs.size(); view_idx++) {
+        const auto g = views[view_idx].view->get_geometry();
+        // TODO we're calculating this multiple times for no reason
+        float scale = compute_window_scale(g);
+        float width = g.width * scale;
+        float height = g.height * scale;
+        row.full_height = std::max(row.full_height, height);
+
+        if (keep_same_row(row, width, ideal_row_width) || (i == num_rows - 1)) {
+          row.view_idxs.push_back(view_idx);
+          row.full_width += width;
+        } else {
+          break;
+        }
+      }
+
+      rows.push_back(row);
+    }
+
+    float grid_height = 0.0;
+    QWFOverviewRow *max_row = nullptr;
+    for (auto &row : rows) {
+      sort_horizontal(row.view_idxs);
+
+      if (!max_row || row.full_width > max_row->full_width) {
+        max_row = &row;
+      }
+      grid_height += row.full_height;
+    }
+    int max_columns = max_row->view_idxs.size();
+    float grid_width = max_row->full_width;
+
+    float scale, space;
+    std::tie(scale, space) = compute_scale_and_space(num_rows, max_columns, grid_width, grid_height);
+
+    // TODO loop with rows and _isBetterScaleAndSpace
+    // this is required for trying all row combinations
+
+    // TODO logic from _getWindowSlots()
+
+    compute_window_slots(rows, scale);
+  }
+
+  std::tuple<float, float> compute_scale_and_space(int num_rows, int num_cols, float grid_width, float grid_height) {
+    const auto screen = output->get_screen_size();
+
+    float h_spacing = (num_cols - 1) * col_spacing;
+    float v_spacing = (num_rows - 1) * row_spacing;
+
+    float spaced_width = (float)screen.width - h_spacing;
+    float spaced_height = (float)screen.height - v_spacing;
+
+    float h_scale = spaced_width / grid_width;
+    float v_scale = spaced_height / grid_height;
+
+    float scale = std::min(std::min(h_scale, v_scale), window_preview_max_scale);
+
+    float scaled_layout_width = grid_width * scale + h_spacing;
+    float scaled_layout_height = grid_height * scale + v_spacing;
+    float space = (scaled_layout_width * scaled_layout_height) / (screen.width * screen.height);
+
+    return std::make_tuple(scale, space);
+  }
+
+  void compute_window_slots(std::vector<QWFOverviewRow> &rows, float scale) {
+
+    compute_row_sizes(rows, scale);
+
+    float height_without_spacing = 0.0;
+    for (const auto &row : rows) {
+      height_without_spacing += row.height;
+    }
+
+    const auto screen = output->get_relative_geometry();
+
+    float v_spacing = (rows.size() - 1) * row_spacing;
+    float extra_v_scale = std::min(1.0f, (screen.height - v_spacing) / height_without_spacing);
+
+    float compensation = 0.0;
+
+    float y = 0.0;
+    for (auto &row : rows) {
+      float h_spacing = (row.view_idxs.size() - 1) * col_spacing;
+      float width_without_spacing = row.width - h_spacing;
+      float extra_h_scale = std::min(1.0f, (screen.width - h_spacing) / width_without_spacing);
+
+      if (extra_h_scale < extra_v_scale) {
+        row.extra_scale = extra_h_scale;
+        compensation += (extra_v_scale - extra_h_scale) * row.height;
+      } else {
+        row.extra_scale = extra_v_scale;
+      }
+
+      row.x = screen.x + std::max(0.0f, (screen.width - (width_without_spacing * row.extra_scale + h_spacing)) / 2.0f);
+      row.y = screen.y + y + std::max(0.0f, (screen.height - (height_without_spacing + v_spacing)) / 2.0f);
+      y += row.height * row.extra_scale + row_spacing;
+    }
+
+    compensation /= 2.0;
+
+    for (auto &row : rows) {
+      float row_y = row.y + compensation;
+      float row_height = row.height * row.extra_scale;
+
+      float x = row.x;
+      for (const int view_idx : row.view_idxs) {
+        auto &sv = views[view_idx];
+        const auto view_g = sv.view->get_geometry();
+
+        float cell_scale = scale * compute_window_scale(view_g) * row.extra_scale;
+        float cell_width = view_g.width * cell_scale;
+        float cell_height = view_g.height * cell_scale;
+
+        {
+          float transform_scale = std::min(cell_scale, window_preview_max_scale);
+
+          // geometry_t uses int, since we want to align with the pixel grid anyway it's fine that we lose precision here
+          auto transform_g = wf::geometry_t{};
+          transform_g.width = cell_width;
+          transform_g.height = cell_height;
+          transform_g.x = x;
+          if (rows.size() == 1) {
+            transform_g.y = row_y + (row_height - cell_height) / 2.0;
+          } else {
+            transform_g.y = row_y + row_height - cell_height;
+          }
+
+          auto offset = get_center(transform_g) - get_center(view_g);
+
+          // TODO when trying for the best layout we probably have to extract this so the transition is only modified once
+          // floor values to align with pixel grid
+          reposition(sv, offset.round_down(), transform_scale);
+        }
+
+        x += cell_width + col_spacing;
+      }
+    }
+  }
+
+  float compute_window_scale(const wf::geometry_t &g) {
+    const auto screen = output->get_screen_size();
+    float ratio = (float)g.height / (float)screen.height;
+    return lerp(1.5, 1.0, ratio);
+  }
+
+  float lerp(float a, float b, float t) {
+    return a * (1.0 - t) + (b * t);
+  }
+
+  void compute_row_sizes(std::vector<QWFOverviewRow> &rows, float scale) {
+    for (auto &row : rows) {
+      row.width = row.full_width * scale + (row.view_idxs.size() - 1) * col_spacing;
+      row.height = row.full_height * scale;
+    }
+  }
+
+  bool keep_same_row(const QWFOverviewRow &row, float width, float idealRowWidth) {
+    if (row.full_width + width <= idealRowWidth) {
+      return true;
+    }
+
+    float oldRatio = row.full_width / idealRowWidth;
+    float newRatio = (row.full_width + width) / idealRowWidth;
+
+    if (std::abs(1 - newRatio) < std::abs(1 - oldRatio)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  void reposition(QWFOverviewView &sv, wf::point_t offset, float scale) {
+    sv.attribs.off_x.restart_with_end(offset.x);
+    sv.attribs.off_y.restart_with_end(offset.y);
+    sv.attribs.scale_x.restart_with_end(scale);
+    sv.attribs.scale_y.restart_with_end(scale);
+  }
+
+  /* Sort windows horizontally to minimize travel distance.
+   * This affects in what order the windows end up in a row. */
+  void sort_horizontal(std::vector<int> &view_idxs) {
+    std::sort(view_idxs.begin(), view_idxs.end(), [this](int a, int b) {
+      auto ag = views[a].view->get_geometry();
+      auto bg = views[b].view->get_geometry();
+      float a_center_x = ag.x + ag.width / 2.0;
+      float b_center_x = bg.x + bg.width / 2.0;
+      return a_center_x < b_center_x;
+    });
+  }
+
+  /* Sort windows vertically to minimize travel distance.
+   * This affects what rows the windows get placed in. */
+  void sort_vertical(std::vector<int> &view_idxs) {
+    std::sort(view_idxs.begin(), view_idxs.end(), [this](int a, int b) {
+      auto ag = views[a].view->get_geometry();
+      auto bg = views[b].view->get_geometry();
+      float a_center_y = ag.y + ag.height / 2.0;
+      float b_center_y = bg.y + bg.height / 2.0;
+      return a_center_y < b_center_y;
+    });
+  }
+
+  wf::pointf_t get_center(wf::geometry_t g) {
+    return {
+        g.x + g.width / 2.0,
+        g.y + g.height / 2.0,
+    };
   }
 
   void dearrange() {
@@ -408,6 +643,7 @@ public:
         view->store_data(std::make_unique<wf::custom_data_t>(), "qwf-overview-minimized-showed");
       }
 
+      // TODO make this a 3d transformer again so the windows have a z order and don't go through each other
       view->get_transformed_node()->add_transformer(
           std::make_shared<wf::scene::view_2d_transformer_t>(view),
           wf::TRANSFORMER_2D, window_transformer_name);
@@ -435,6 +671,8 @@ public:
     auto transform = sv.view->get_transformed_node()
                          ->get_transformer<wf::scene::view_2d_transformer_t>(window_transformer_name);
     assert(transform);
+
+    LOGI("qwf: render x ", sv.attribs.off_x);
 
     transform->translation_x = sv.attribs.off_x;
     transform->translation_y = sv.attribs.off_y;
